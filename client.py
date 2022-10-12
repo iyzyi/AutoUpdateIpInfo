f15b22f1533a78f5c89553cd6b905cd68f068ea1
iyzyi <49014178+iyzyi@users.noreply.github.com>
2022-10-12 13:47:47 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import os, requests
+import os, requests, time
 from time import sleep
 
 password = 123456789
@@ -9,10 +9,15 @@ while (True):
     ip_info = ip_info.replace('\n', '<br>')
     cmd.close()
 
+    ip_info = '更新时间：' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '<br><br>' + ip_info
+
     url = 'http://127.0.0.1:12345'
     data = {'password': password, 'ip_info': ip_info}
-    res = requests.post(url, data = data)
-    print(res.text)
+    try:
+        res = requests.post(url, data = data)
+        print(res.text)
+    except Exception as e:
+        print('出错：', e)
 
     # 一分钟更新一次
     sleep(60)
